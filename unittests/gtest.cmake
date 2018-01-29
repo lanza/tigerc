6e36a966e1bf5ea428abc99602d7cdb7e1204cec
Nathan Lanza <lanza@fb.com>
2018-01-28 16:29:11 -0800

    [unittests] convert gtest to be a compile time download and build

--- unittests/gtest.cmake
+++ unittests/gtest.cmake
@@ -1,22 +1,39 @@
-# Download and unpack googletest at configure time
-configure_file(CMakeLists.txt.in ${CMAKE_BINARY_DIR}/googletest-download/CMakeLists.txt)
-message(${CMAKE_COMMAND} ${CMAKE_GENERATOR})
-execute_process(COMMAND ${CMAKE_COMMAND} -G "${CMAKE_GENERATOR}" .
-  RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
-if(result)
-  message(FATAL_ERROR "CMake step for googletest failed: ${result}")
-endif()
-execute_process(COMMAND ${CMAKE_COMMAND} --build .
-  RESULT_VARIABLE result
-  WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/googletest-download )
-if(result)
-  message(FATAL_ERROR "Build step for googletest failed: ${result}")
-endif()
+find_package(Threads)
+include(ExternalProject)
 
-# Add googletest directly to our build. This defines
-# the gtest and gtest_main targets.
-add_subdirectory(${CMAKE_BINARY_DIR}/googletest-src
-                 ${CMAKE_BINARY_DIR}/googletest-build
-                 EXCLUDE_FROM_ALL)
+ExternalProject_Add(googletest
+	URL https://github.com/google/googletest/archive/master.zip
+	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+	INSTALL_COMMAND ""
+	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/unittests/gtest/src/googletest-build/googlemock/gtest/libgtest_main.a
+	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/unittests/gtest/src/googletest-build/googlemock/gtest/libgtest.a
+	BUILD_BYPRODUCTS ${CMAKE_BINARY_DIR}/unittests/gtest/src/googletest-build/googlemock/libgmock.a
+)
 
+ExternalProject_Get_Property(googletest binary_dir)
+
+add_library(gtest UNKNOWN IMPORTED)
+add_dependencies(gtest googletest)
+set_target_properties(gtest PROPERTIES
+	"IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+	"IMPORTED_LOCATION" ${binary_dir}/googlemock/gtest/libgtest.a
+)
+
+add_library(gmock UNKNOWN IMPORTED)
+add_dependencies(gmock googletest)
+set_target_properties(gmock PROPERTIES
+	"IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+	"IMPORTED_LOCATION" ${binary_dir}/googlemock/libgmock.a 
+)
+
+add_library(gtest_main UNKNOWN IMPORTED)
+add_dependencies(gtest_main googletest)
+set_target_properties(gtest_main PROPERTIES
+	"IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+	"IMPORTED_LOCATION" ${binary_dir}/googlemock/gtest/libgtest_main.a
+)
+
+include_directories(
+	${CMAKE_CURRENT_BINARY_DIR}/gtest/src/googletest/googlemock/include
+	${CMAKE_CURRENT_BINARY_DIR}/gtest/src/googletest/googletest/include
+)
